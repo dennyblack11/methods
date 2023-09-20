8a5d920074871e68f24e8cfcfc2b86b830afa031
dennyblack11 <dennisozoemena3@gmail.com>
2023-09-20 08:13:17 +0100

    Practice eight(8)

--- newHttp.ts
+++ newHttp.ts
@@ -1,110 +1,100 @@
-import http, { IncomingMessage, ServerResponse } from "http"
-
-const port: number = 5500
+import http,{ IncomingMessage, ServerResponse } from "http"
 
 interface iData{
-    id: number,
-    gender: string,
-    Complexion: string,
-    height: string,
-    marital_status: string,
+    id: number
+    name: string
+    phoneNum: number
+    stack: string
 }
 
-interface iMessage {
-    message: string,
-    success: boolean,
-    data: null|{}[],
+
+interface iMessage{
+    message: string
+    success: boolean
+    data: null|{}|{}[]
 }
 
-const Information: {}[] = [
+const set08: {}[] = [
     {
         id: 1,
-        gender: "Female",
-        complexion: "Fair",
-        height: "Average",
-        marital_status: "Single"
+        name: "Dennis",
+        phoneNum: 8086247067,
+        stack: "full-stack"
     },
     {
         id: 2,
-        gender: "Male",
-        complexion: "Fair",
-        height: "Tall",
-        marital_status: "Married"
+        name: "Jemima",
+        phoneNum: 7036921433,
+        stack: "middle-stack"
     },
     {
         id: 3,
-        gender: "Female",
-        complexion: "Dark",
-        height: "Short",
-        marital_status: "Single"
+        name: "Ayo",
+        phoneNum: 8086247067,
+        stack: "full-stack"
     },
     {
         id: 4,
-        gender: "Male",
-        complexion: "Dark",
-        height: "Average",
-        marital_status: "Single"
+        name: "Tobi",
+        phoneNum: 8086247067,
+        stack: "front-end"
     },
     {
         id: 5,
-        gender: "Female",
-        complexion: "Fair",
-        height: "Tall",
-        marital_status: "Married"
-    },
+        name: "Ayo",
+        phoneNum: 8086247067,
+        stack: "back-end"
+    }
 ]
 
-const server = http.createServer ((req:IncomingMessage, res:ServerResponse<IncomingMessage>) => {
+const port: number = 5000
+
+const server = http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>) => {
     res.setHeader("Content-Type", "Application/JSON")
 
-    const {url, method} = req
+    const {method, url} = req
+    let status: number = 404
 
-    let status = 404;
-
-    let getBack: iMessage = {
+    let respond: iMessage = {
         message: "failed",
         success: false,
         data: null
-    }
-
-    const Father: any = [];
-
-    req.on("data", (chunk: any) => {
-        Father.push(chunk)
+    };
+    const container: any = [];
+    req.on("data", (chunk: any ) => {
+        container.push(chunk)
     }).on("end", () => {
-
-        //GET METHOD
-
+        //Get Method
         if(url === "/" && method === "GET"){
             status = 200;
+            respond.message = "All set08 data gotten";
+            respond.success = true;
+            respond.data = set08;
+            res.write(JSON.stringify({status, respond}));
 
-            getBack.message = "All Information Gotten";
-            getBack.success = true;
-            getBack.data = Information;
-
-            res.write(JSON.stringify ({status, getBack}))
-
-            res.end()
+            res.end();
         }
 
-        //POST METHOD
-
+        //Post Method
         if(url === "/" && method === "POST"){
-            status = 201
-            const body = JSON.parse(Father)
-            Information.push(body);
-            getBack.message = "Successfully Added";
-            getBack.success = true;
-            getBack.data = Information
+            status = 201;
+            const body = JSON.parse(container);
+        set08.push(body);
+        respond.message = "SUCCESSFULLY ADDED";
+        respond.success = true;
+        respond.data = set08;
+        res.write(JSON.stringify({
+        status, respond}))
 
-            res.write(JSON.stringify ({status, getBack}))
-
-            res.end()
+        res.end()
         }
+
+        //patch method
+        //put method
     })
+    // res.end()
 })
 
 server.listen(port, () => {
-    console.log("Server is up and running");
-    
-})
+    console.log("Server is up and running")
+}
